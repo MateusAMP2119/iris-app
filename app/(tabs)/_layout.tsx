1b7d7a7b19c3c5daf90f8fe5842a1f12eba32507
Mateus Costa <105391128+MateusAMP2119@users.noreply.github.com>
2025-12-02 08:16:23 +0000

    added labels to nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,21 @@
-import { Icon, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   return (
     <NativeTabs minimizeBehavior="onScrollDown">
       <NativeTabs.Trigger name="index">
         <Icon sf="calendar" drawable="ic_menu_today" />
+        <Label>Today</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="foryou">
         <Icon sf="person.fill" drawable="ic_menu_person" />
+        <Label>For You</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="forlater">
         <Icon sf="bookmark.fill" drawable="ic_menu_bookmark" />
+        <Label>For Later</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="search" role="search">
